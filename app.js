dc968ac83aaf2a2408afe06b6dacd0d76ef73ff7
Matthew Dodd <matthew.dodd93@gmail.com>
2019-01-12 22:55:51 +0100

    Edit to comments

--- app.js
+++ app.js
@@ -14,16 +14,18 @@ app.use('/static', express.static('public'));
 
 //------Routing---------//
 
-//index
+//index route. Project will hold the array of projects
 app.get('/', (req, res, next) => {
     const project = json.projects;
     res.render('index', {project:project});
 });
 
+// /about route. Will render about.pug
 app.get('/about', (req, res) => {
     res.render('about');
 });
 
+// /project route. Id will be the index of project to show, will then render project.pug passing in all relevant project info into the template
 app.get('/project/:id', (req, res) => {
     let {id} = req.params;
     const project = json.projects[id];
@@ -44,12 +46,15 @@ app.get('/project/:id', (req, res) => {
     });
 });
 
+//Error handler for 404 issues, will pass err object to error handler below
 app.use((req, res, next) => {
     const err = new Error('We cannot find this page');
     err.status = 404;
     next(err);
 });
 
+
+//Error handler. will render the error.pug template.
 app.use((err, req, res, next) => {
     res.status(err.status||500);
     console.log(`Oh no-- ${err.message}  Error:${res.statusCode}`);
